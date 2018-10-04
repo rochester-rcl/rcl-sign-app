30ee09287d6d75344b7db5c769fe3ba2437131ed
jromphf <jromphf@library.rochester.edu>
2018-10-04 15:23:09 -0400

    added translation at the router level so all paths work properly

--- src/containers/RouterContainer.js
+++ src/containers/RouterContainer.js
@@ -1,34 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import { Router, Route } from 'react-router-dom';
+import { Router, Route } from "react-router-dom";
 
 // Redux
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 
 // history
 import history from "../utils/history";
 
 // Actions
-import * as AppActions from '../actions/Actions';
+import * as AppActions from "../actions/Actions";
 
 // Containers
 
-import DictionaryContainer from './DictionaryContainer';
+import DictionaryContainer from "./DictionaryContainer";
 
 // Components
-import StaticPage from '../components/StaticPage';
+import StaticPage from "../components/StaticPage";
 
-import Navigation from '../components/Navigation';
+import Navigation from "../components/Navigation";
 
-import Loading from '../components/Loader';
+import Loading from "../components/Loader";
 
 export class AppRouter extends Component {
   _element = React.createElement;
 
+  state = { routeTranslations: { en: {}, fr: {} } };
+
   constructor(props: Object) {
     super(props);
     this.renderStatic = this.renderStatic.bind(this);
+    this.initRouteTranslations = this.initRouteTranslations.bind(this);
+    this.translationRedirect = this.translationRedirect.bind(this);
   }
 
   componentDidMount() {
@@ -36,9 +40,54 @@ export class AppRouter extends Component {
     this.staticRoutes = [];
   }
 
+  componentDidUpdate(prevProps: Object) {
+    if (prevProps.nav === null && this.props.nav !== null) {
+      this.initRouteTranslations();
+    }
+    if (prevProps.language !== this.props.language) {
+      this.translationRedirect();
+    }
+  }
+
+  initRouteTranslations(): void {
+    if (this.props.nav === null)
+      console.warn(
+        "Route Translations can't be initialized before navigation is loaded!"
+      );
+    const { en, fr } = this.props.nav;
+    const translationsEn = {};
+    const translationsFr = {};
+    en.forEach((item, index) => {
+      let frPath = fr[index].path;
+      translationsFr[item.path] = frPath;
+      translationsEn[frPath] = item.path;
+    });
+    this.setState({
+      routeTranslations: {
+        en: translationsEn,
+        fr: translationsFr
+      }
+    });
+    // set language based on URL
+    const { pathname } = history.location;
+    const path = pathname.substring(1);
+    if (translationsEn[path] !== undefined) {
+      this.props.setAppLanguageAction("fr");
+    }
+  }
+
+  translationRedirect(): void {
+    const { pathname } = history.location;
+    const { routeTranslations } = this.state;
+    const { language } = this.props;
+    // remove slash from path
+    const path = pathname.substring(1);
+    history.push(routeTranslations[language][path]);
+  }
+
   renderStatic(ownProps: Object, route: Object) {
     const { path, language, ...props } = route;
-    return this._element(StaticPage, {...ownProps, ...props}, null);
+    return this._element(StaticPage, { ...ownProps, ...props }, null);
   }
 
   render() {
@@ -47,23 +96,30 @@ export class AppRouter extends Component {
     if (nav !== null) {
       items = nav[language];
       if (this.staticRoutes.length === 0) {
-        this.staticRoutes = nav.en.filter((item) => item.static === true).concat((nav.fr.filter((item) => item.static === true)));
+        this.staticRoutes = nav.en
+          .filter(item => item.static === true)
+          .concat(nav.fr.filter(item => item.static === true));
       }
     }
     if (items == null) {
-      return (
-        <Loading text="Loading ..." />
-      )
+      return <Loading text="Loading ..." />;
     } else {
-      return(
+      return (
         <Router history={history}>
           <div className="lsf-app-root-container">
-            <Navigation items={items} language={language} handleSelectLanguage={setAppLanguageAction} />
+            <Navigation
+              items={items}
+              language={language}
+              handleSelectLanguage={setAppLanguageAction}
+            />
             <Route path="/dictionary" component={DictionaryContainer} />
             <Route path="/dictionnaire" component={DictionaryContainer} />
-            {this.staticRoutes.map((route) =>
-              <Route path={'/' + route.path} render={(props) => this.renderStatic(props, route)} />
-            )}
+            {this.staticRoutes.map(route => (
+              <Route
+                path={"/" + route.path}
+                render={props => this.renderStatic(props, route)}
+              />
+            ))}
           </div>
         </Router>
       );
@@ -89,12 +145,15 @@ function mapStateToProps(state): Object {
     layoutAspect: state.layoutAspect,
     searchResults: state.searchResults,
     introText: state.introText,
-    nav: state.nav,
-  }
+    nav: state.nav
+  };
 }
 
 function mapActionCreatorsToProps(dispatch) {
   return bindActionCreators(AppActions, dispatch);
 }
 
-export default connect(mapStateToProps, mapActionCreatorsToProps)(AppRouter);
+export default connect(
+  mapStateToProps,
+  mapActionCreatorsToProps
+)(AppRouter);
